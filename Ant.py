d6737274089c74e57084bf7cba1dd035c7910338
T-maruNa <tomaru.naoki@gmail.com>
2018-04-16 23:17:51 +0900

    最小までの計算をコミット クラスはいらなかったので涙の削除

--- Ant.py
+++
@@ -1,8 +0,0 @@
-class Ant:
-    __position = 0
-    __vector = 0
-    def __init__(self, position):
-        self.__position = position
-
-    def show(self):
-        print(self.__position)
--- Main.py
+++ Main.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
-import Ant
+# 最小を計算
+def minMove():
+    minList = []
+    maxCnt = 0
+    for ant in antList:
+        if(ant - (lineSize / 2) > 0):
+            minList.append("右")
+            outPoint = lineSize - ant
+        else:
+            minList.append("左")
+            outPoint = ant
 
-antList = []
-positionList = [2,6,7]
+        if(outPoint > maxCnt):
+            maxCnt = outPoint
+
+    print('%d(%s)' % (maxCnt , ','.join(minList)))
+
+
+# ---Main処理---
+lineSize = 10
+antList = [2,6,7]
 
 # 位置の昇順にソート
-positionList.sort()
-
-for p in positionList:
-    antList.append(Ant.Ant(p))
-
-for ant in antList:
-    ant.show()
+antList.sort()
+minMove()
