660c0223900d4902aae9df9130614d5e56341631
T-maruNa <tomaru.naoki@gmail.com>
2018-04-18 00:21:52 +0900

    POJ No.185 チャレンジ終了

---
+++ Ants_POJ_No1852.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------
+# POJ No.1852
+# 長さLcmの竿の上をn匹の蟻が毎秒1cm/sの速さで歩いています．
+# 蟻が竿の端に達すると竿の下に直ちに落ちていきます．
+# (竿の上は狭くてすれ違うことができないので)2匹の蟻が出会うと，
+# それぞれ反対の方向を向いて歩き始めます．
+# 各蟻iについて，初期位置(左端からの距離 xi)は分かりますが，
+# 残念なことに，どちらの方向に向かって歩いているのかは分かりません．
+# すべての蟻が竿から落ちるまでにかかる最小の時間と最大の時間をそれぞれ求めてください．
+# -------------------------------------------------------------
+
+# 最小を計算
+def minMove():
+    minList = []
+    maxCnt = 0
+    for ant in antList:
+        if(ant - midP > 0):
+            minList.append("右")
+            outPoint = lineSize - ant
+        else:
+            minList.append("左")
+            outPoint = ant
+
+        if(outPoint > maxCnt):
+            maxCnt = outPoint
+
+    print('%d(%s)' % (maxCnt , ','.join(minList)))
+
+# 最大を取得
+# --いろいろグダグダ考えたが、出会った方が時間がかかるパターンはない
+# --最小を逆の計算で良い
+def maxMove():
+    # 左端,右端を取得
+    first = 0
+    last = len(antList) - 1
+    maxL = lineSize - antList[first]
+    maxR = antList[last]
+    maxList = []
+
+    if(maxL > maxR):
+        maxCnt = maxL
+        vector = "右"
+    else:
+        maxCnt = maxR
+        vector = "左"
+
+    for i in range(len(antList)):
+        maxList.append(vector)
+
+    print('%d(%s)' % (maxCnt , ','.join(maxList)))
+
+# ---Main処理---
+lineSize = 10
+antList = [2,6,7]
+midP = lineSize / 2
+
+# 位置の昇順にソート
+antList.sort()
+minMove()
+maxMove()
